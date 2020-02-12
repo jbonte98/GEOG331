66b95ac54701e98d30ee074259cd77c2f9ae5476
Jeremy Bonte '20 <jbonte@colgate.edu>
2020-02-12 12:10:09 -0500

    Finished with day one of class

--- activity3/activity3_script.R
+++ activity3/activity3_script.R
@@ -1,3 +1,6 @@
+#install.packages(c("lubridate"))
+library(lubridate)
+
 #create a function. The names of the arguements for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
 assert <- function(statement,err.message){
   #if evaluates if a statement is true or false for a single item
@@ -15,3 +18,63 @@ assert(2 == 2, "error: unequal values")
 a <- c(1,2,3,4)
 b <- c(8,4,5)
 assert(length(a) == length(b), "error: unequal length")
+
+#read in the data file
+#skip the first 3 rows since there is additional column info
+#specify the the NA is designated differently
+datW <- read.csv("y:\\Students\\hkropp\\a03\\bewkes_weather.csv",
+                 na.strings=c("#N/A"), skip=3, header=FALSE)
+#preview data
+print(datW[1,])
+#get sensor info from file
+# this data table will contain all relevent units
+sensorInfo <-   read.csv("y:\\Students\\hkropp\\a03\\bewkes_weather.csv",
+                         na.strings=c("#N/A"), nrows=2)
+
+print(sensorInfo)
+#get column names from sensorInfo table
+# and set weather station colnames  to be the same
+colnames(datW) <-   colnames(sensorInfo)
+#preview data
+print(datW[1,])
+
+#convert to standardized format
+#date format is m/d/y
+dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calcualtions
+datW[1,]
+
+#see how many values have missing data for each sensor observation
+#air temperature
+length(which(is.na(datW$air.temperature)))
+#wind speed
+length(which(is.na(datW$wind.speed)))
+#precipitation
+length(which(is.na(datW$precipitation)))
+#soil moisture
+length(which(is.na(datW$soil.moisture)))
+#soil temperature
+length(which(is.na(datW$soil.temp)))
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+#check the values at the extreme range of the data
+#and throughout the percentiles
+quantile(datW$air.tempQ1)
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,]
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,]  
